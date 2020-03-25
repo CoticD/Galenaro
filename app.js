e4893ff4b577fae877db4fc41dabe325dc11715c
Domen Cotič <domen.cotic@gmail.com>
2020-03-25 17:53:10 +0100

    Updated files for heroku use

--- app.js
+++ app.js
@@ -10,15 +10,10 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(){
-  var firstName = req.body.firstName;
-  var lastName = req.body.lastName;
-  var email = req.body.email;
-});
-
-app.listen(3000, function(){
+app.listen(process.env.PORT || 3000, function(){
   console.log("Server is running on port 3000");
+  console.log(__dirname + "/test.html");
 });
